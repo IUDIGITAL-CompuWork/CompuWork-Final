777c05b3ac9cab4e3c1d1476089233d4ac571b68
Juanda <juan.arboledaz@est.iudigital.edu.co>
2024-10-06 18:04:03 -0500

    Creación de las clases Main y Departamento con su respectivo código Java.

---
+++ SRC/Departamento.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package departamento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Departamento {
+    private int id;
+    private String nombre;
+    private String jefe;
+    private String descripcion;
+    private List<Empleado> empleados;
+
+    public Departamento(int id, String nombre, String jefe, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.jefe = jefe;
+        this.descripcion = descripcion;
+        this.empleados = new ArrayList<>();
+    }
+
+    public void agregarEmpleado(Empleado empleado) {
+        empleados.add(empleado);
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getJefe() {
+        return jefe;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setNombre(String nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+
+    public void setJefe(String nuevoJefe) {
+        this.jefe = nuevoJefe;
+    }
+
+    public void setDescripcion(String nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
+    }
+}
