9989d9480095c0ec23ca7b7038a927d14d358d72
Oscar Jimenez <oscar.jimenez@est.iudigital.edu.co>
2024-10-06 17:59:25 -0500

    Codigo creado de la clase EmpleadoPermanente, elaborado por Tatiana Gallego.

--- SRC/EmpleadoPermanente.java
+++ SRC/EmpleadoPermanente.java
@@ -1,2 +1,39 @@
-public class EmpleadoPermanente {
+import javax.swing.JTextArea;
+
+public class EmpleadoPermanente extends Empleado {
+    private String beneficios;
+    private double salarioBase;
+
+    public EmpleadoPermanente(int id, String nombre, int edad, String sexo, String fechaContratacion, String beneficios, double salarioBase) {
+        super(id, nombre, edad, sexo, fechaContratacion);
+        this.beneficios = beneficios;
+        this.salarioBase = salarioBase;
+    }
+
+    @Override
+    public void mostrarDetalles(JTextArea displayArea) {
+        displayArea.append("Empleado Permanente - ID: " + getId() +
+                ", Nombre: " + getNombre() +
+                ", Edad: " + getEdad() +
+                ", Sexo: " + getSexo() +
+                ", Fecha de Contratación: " + getFechaContratacion() +
+                ", Beneficios: " + beneficios +
+                ", Salario Base: " + salarioBase + "\n");
+    }
+
+    public String getBeneficios() {
+        return beneficios;
+    }
+
+    public void setBeneficios(String beneficios) {
+        this.beneficios = beneficios;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
 }
