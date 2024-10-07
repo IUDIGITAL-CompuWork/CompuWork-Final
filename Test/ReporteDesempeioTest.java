9e369dbf9f370a1b7c6160c9fce766e07f50bf90
Oscar Jimenez <oscar.jimenez@est.iudigital.edu.co>
2024-10-07 09:33:02 -0500

    Creando prueba unitaria en JUnit de la clase ReporteDesempeio, elaborado por Oscar Jimenez.

---
+++ Test/ReporteDesempeioTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReporteDesempeioTest {
+
+    @Test
+    void getFechaGeneracion() {
+    }
+
+    @Test
+    void getPuntaje() {
+    }
+
+    @Test
+    void getObservaciones() {
+    }
+
+    @Test
+    void getEmpleado() {
+    }
+
+    @Test
+    void getDepartamento() {
+    }
+}
